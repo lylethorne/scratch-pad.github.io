c88e07f77cb65588406ce873add52bb8ed8a0fcd
lylethorne <lylegthorne@gmail.com>
2024-03-30 16:54:50 -0500

    STUDIES SET UP

---
+++ Studies/variables.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * VARIABLES:
+ * 
+ * 0. INTRO HEREE
+ * 
+ * 1. Declaration and assignment definition here...
+ * 
+ * 2. var, let, and const here... 
+ * 
+ * 3. hoisting here
+ * 
+ */
+
+
+//Declaration here
+var name;
+
+///assignment here
+var name = Lyle;
+
+//var let and const coding here
