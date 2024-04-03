3938aa94506a4d56234009522b83b145191d0508
lylethorne <lylegthorne@gmail.com>
2024-04-02 20:56:40 -0500

    functions

---
+++ Studies/functions.js
@@ -0,0 +1,37 @@
+/**
+ * function bb
+ * 
+ * function is a block of code that gets reused - keeping it dry and cute. 
+ * 
+ * Functions are made up of a name, parameters aka arguments, the function body,
+ * and sometimes but not always a return statement. A function can be defined AND called 
+ * - but the two are not the same! 
+ * 
+ * functions can be created by defining or expressing
+ * 
+ * //define
+ * function name(parameter){
+ *  body
+ *  body
+ *  maybe a return
+ * }
+ * 
+ * A function Expression is when a variable's value is a function
+ * 
+ * var x = function name(parameter){ body }
+ * 
+ * 
+ * //call
+ * name(parameter);
+ * 
+ * Functions are treated as first-class objects in Javascript! 
+ * First-class objects can be a parameter for a function, returned by a function,
+ * and even stored as a variable (but you already know that, because you've read expressions), 
+ * but also inside arrays and objects!
+ * 
+ * When a function is used as a function's parameter - it is a 'callback'.
+ * 
+ * 
+ * 
+ * 
+ */
