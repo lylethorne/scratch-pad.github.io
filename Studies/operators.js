d3444050fb8d18431dc38b8d771b7092469f17c3
lylethorne <lylegthorne@gmail.com>
2024-04-07 18:27:12 -0500

    control flow

--- Studies/operators.js
+++ Studies/operators.js
@@ -1,11 +1,19 @@
-///YOU NEED A CODED EXAMPLE FOR EVERY SINGLE TYPE OF OPERATOR EXCEPT URNAIRY
+///YOU NEED A CODED EXAMPLE FOR EVERY SINGLE TYPE OF OPERATOR EXCEPT UNARY
 /**
  * 
  * 0.
  * 
- * 1.
+ * 1. Assignment operators
  * 
- * 2. 
+ * 2. Arithmetic operators
+ * 
+ * 3. Comparison operators
+ * 
+ * 4. Logical operators
+ * 
+ * 5. Unary operators
+ * 
+ * 6. Ternary operators
  * 
  * 
  */
--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -1,1 +1,11 @@
 //4 TOTAL CODED EXAMPLES OF STRING MANIP WITH OPERATORS AND WITH STRING METHODS
+/**
+ * 
+ * 0.
+ * 
+ * 
+ * 1. With operators
+ * 
+ * 2. With string methods
+ * 
+ */
