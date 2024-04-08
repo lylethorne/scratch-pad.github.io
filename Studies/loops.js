1b6003efef899b272da3f27452e4d045a0160520
lylethorne <lylegthorne@gmail.com>
2024-04-07 19:36:57 -0500

    operators

---
+++ Studies/loops.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * 0. Loops
+ * 
+ * 1. For/for in / while loops
+ * 
+ * 2. Loops can count forward any number or pattern, or backwards to 0.
+ * 
+ * 3. Loop over array forwards or backwards
+ * 
+ * 4. Loop over an object
+ */
--- Studies/operators.js
+++ Studies/operators.js
@@ -1,15 +1,28 @@
 ///YOU NEED A CODED EXAMPLE FOR EVERY SINGLE TYPE OF OPERATOR EXCEPT UNARY
 /**
+ * OPERATOR
  * 
- * 0.
+ * 0. An Operator is a type of syntax that consists of alphabetical characters, punctuation, and integers.
+ * These characters are reserved in the program because they have a built in functionality.
  * 
- * 1. Assignment operators
+ * 1. Assignment operators are used when variables are being assigned '=' to data. They can also be
+ * used to manipulate the data by addition '+=' and subtraction '-='.
  * 
- * 2. Arithmetic operators
+ * 2. Arithmetic operators allow math to perform in code. There is add '+', subtract '-', 
+ * division '/', mulitiplication '*', exponentiation '**', remainder '%', increment '++', 
+ * and decrement '--'. These can be performed on numbers, variables, and expressions. And,
+ * just as within mathmatics - when multiple arithmetic operators are present, operator 
+ * precedence takes place (multiplication is done before addition, etc...).
  * 
- * 3. Comparison operators
+ * 3. Comparison operators are used to determine equity or diference in logical statements.
+ * Generally the result of using one of these will result in a true or false value.
+ * There is equal to '==', equal value and type '===', not equal to '!=', not equal to value
+ * nor type '!==', greater than '>', less than '<', equal or greather than '>=', equal or 
+ * less than '<='.
  * 
- * 4. Logical operators
+ * 4. Logical operators are used to determine the logic betwen variables or values. There is
+ * and '&&', or '||', not '!'. The first two are used when there are multiple conditions that must
+ * be true or false in order for that next piece of code to run.
  * 
  * 5. Unary operators
  * 
