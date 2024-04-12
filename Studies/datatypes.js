04899d2da94f616cfce0c219802f54f14ba4e461
lylethorne <lylegthorne@gmail.com>
2024-04-12 14:45:13 -0500

    data types

--- Studies/datatypes.js
+++ Studies/datatypes.js
@@ -1,27 +1,45 @@
 /**
- * 0. Data Types
  * 
- * 1. Number
+ * DATA TYPES
  * 
- * 2. String
+ * 0. Data types are a specific value type that the program needs to distinguish from one another. 
+ * They are built in and in order for the program to run, it needs to know what it is operating on
+ * and if it is a simple or complex data type.
  * 
- * 3. Boolean
+ * 1. Number represents numerical data, such as 1, 3.14159, etc...
  * 
- * 4. Array
+ * 2. Strings are used to represent sentences or words and is represented by that text wrapped 
+ * around quotes or backticks ('', "", ``)
  * 
- * 5. Object
+ * 3. Boolean is the term used to represent a value that will either be true or false
  * 
- * 6. Function
+ * 4. Array is a comlex data type that can hold an infinit amount of data. Arrays are thought of
+ * as lists where there isnt a relation to the data it holds or the order. Arrays can hold any type
+ * of data including simple and complex. It is represented by square brackets holding the elements 
+ * seperated by a comma [ x, y]
  * 
- * 7. Undefined
+ * 5. Object is also a complex data type, however it speficially refers to a person/object and the 
+ * data that it holds. Objects have properties inside them that are represented by a key and it's value
+ * pair. An object is represented by the curly brackets { } with the key value pairs separated by a colon
+ * and ending with a comma. 
  * 
- * 8. Null
+ * 6. Functions perform actions on a value it is given.
  * 
- * 9. NaN
+ * 7. When a variable is not assigned a value, it is undefined and the type is undefined. 
  * 
- * 10. Infinity and. - infinity
+ * 8. Null represents the intentional absence of a value. 
  * 
- * 11. Difference between simple and complex data types
+ * 9. NaN stands for Not a Number. It is generally encountered when a mathmatical problem 
+ * does not resolve to a number.
+ * 
+ * 10. Infinity and - infinity are properties of the global object, so they can be accessed from anywhere.
+ * Infinity is different then the numeric idea of infinity. Here, it represents a value that is greater than
+ * any other number (in JavaScript it is when a number reaches 1.797693134862315E+308). And the inverse is
+ * true for -infinity.
+ * 
+ * 11. Simple data types are strings, numbers, booleans, null, and undefined. They represent an immutable value, 
+ * and do not have an action. While complex data types are arrays, objects, and functions. Complex data types are
+ * hold series of data and can perform actions on the data they hold.
  * 
  * 12. Primitive values are copied by value, while complex are copied by reference. 
  * what does that mean and how are they different?
