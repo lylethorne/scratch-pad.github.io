604423fba3abe44636297395ae3260bf7317f8fa
lylethorne <lylegthorne@gmail.com>
2024-04-20 20:49:06 -0500

    loops

--- Studies/loops.js
+++ Studies/loops.js
@@ -17,3 +17,37 @@
  * 
  * 4. Loop over an object
  */
+
+//for loop
+let array = [1, 2, 3, 4, 5];
+//starting at index 0  //ending while i is less than the length of the array  //incrementing by one
+for(let i = 0; i < array.length; i++){
+    //if the value of whatever iteration thru the array remainder of 2 strictly equals 0
+    if(array[i] % 2 === 0){
+        console.log('even!'); //log even
+    }else{ // else its not even
+        console.log('false!'); //log false
+    }
+}
+
+
+//an object with three key value pairs
+let object = {
+    a: 'apple',
+    b: 'banana',
+    c: 'carrot'
+}
+//for-in loop over object to access the keys and values
+for(var key in object){
+    console.log(key); //will log a, b, c
+    console.log(object[key]); //will log apple, banana, carrot
+}
+
+
+/*while loops are used when you do not necessarialy know how many times the value needs
+to be iterated over */
+let age = 14; //age assigned to 14
+while(age < 16){ //while age is under 16
+    console.log('too young to drive'); //print too young to drive to the console
+    age++; //incrementing age by one each time
+} 
