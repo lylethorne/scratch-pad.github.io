9288619cb772063df66bd42248802118c8938d7d
lylethorne <lylegthorne@gmail.com>
2024-04-23 11:47:04 -0500

    string

--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -1,7 +1,10 @@
 //4 TOTAL CODED EXAMPLES OF STRING MANIP WITH OPERATORS AND WITH STRING METHODS
 /**
  * 
- * 0.
+ * STRING MANIPULATION  
+ * 
+ * 0. Strings are a simple data type that are immutable, however strings can there are methods that can
+ * be enacted on them that will return a mutated new string and leave the orginal untouched.
  * 
  * 
  * 1. With operators
@@ -9,3 +12,9 @@
  * 2. With string methods
  * 
  */
+
+//charAt() will return whatever character is at the number placed within the ()
+let fruit = 'apple'; //initiz fruit to apple
+console.log(fruit.charAt(0)) //logging to console to see that the first character in fruit is 'a'
+
+//substring 
