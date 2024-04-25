437caef7695f6dea4c22f24b5cf32e4aa1e18f5e
lylethorne <lylegthorne@gmail.com>
2024-04-25 18:36:40 -0500

    more

--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -7,14 +7,25 @@
  * be enacted on them that will return a mutated new string and leave the orginal untouched.
  * 
  * 
- * 1. With operators
+ * 1. Strings can be manipulated by operators such as + (concatenate) and += (concatenate assignment)
+ * The + acts as similar to addition, one thing to keep in mind is that the first character of the
+ * second string will be added to the last character of the first string - no spaces are betwen the two
+ * unless added in manually. Now, what do you think we could do that with?
  * 
  * 2. With string methods
  * 
  */
+
 
 //charAt() will return whatever character is at the number placed within the ()
 let fruit = 'apple'; //initiz fruit to apple
 console.log(fruit.charAt(0)) //logging to console to see that the first character in fruit is 'a'
 
 //substring 
+
+// + concatenate
+let str1 = 'howdy';
+let str2 = 'pardnr';
+
+console.log(str1 + ' ' + str2); // 'howdy partner'
+
